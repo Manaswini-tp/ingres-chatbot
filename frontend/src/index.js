b8eb3a6f04318c9ae206f7fb57705f61f9662bf3
Manaswini T P <manaswinitp2005@gmail.com>
2026-08-12 20:58:08 +0530

    Wrap App component with ThemeProvider and AuthProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './context/ThemeContext';
+import { AuthProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
